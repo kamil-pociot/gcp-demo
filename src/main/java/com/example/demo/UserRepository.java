76e1653c972a43b04e421ff4d4d72f33643dd05c
Kamil Pociot <kamil.pociot@auto1.com>
2023-10-24 13:33:00 +0200

    cleanup

--- src/main/java/com/example/demo/UserRepository.java
+++ src/main/java/com/example/demo/UserRepository.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import com.google.cloud.spring.data.firestore.FirestoreReactiveRepository;
 import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Mono;
 
 @Repository
 public interface UserRepository extends FirestoreReactiveRepository<User> {
